4eabe2a2bd8ef23acfc9ec62592e4c53afa190f7
haidi hany <dijadija222@gmail.com>
2018-10-16 22:16:15 +0200

    initial commit

---
+++ stp.js
@@ -0,0 +1,7 @@
+function houres(){
+    "use strict";
+    var myAge=19;
+    return myAge*365*24;
+}
+var calc=myAge();
+alert(calc);
---
+++ ttt.js
@@ -0,0 +1,6 @@
+/*to test java*/
+var x=["text","password","submit"];
+for (var i=0; i<=2;i++)
+{
+    document.write("<input type=" +x[i]+ "/>");
+}
